360f310d0b088dab2b1d4f566eb8fe7b28568489
sibiyan <sibiyan9030@gmail.com>
2020-12-22 23:39:10 +0530

    db change

--- django_project/settings.py
+++ django_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'alsdhflakgjdlakghaldkghalekgoihkjnawekjfnqkwgejh'
 # DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-is-awesome.herokuapp.com']
+ALLOWED_HOSTS = ['django-is-awesome.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -84,8 +84,8 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME': 'Blogdb',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'NAME': 'Blogdb',
     }
 }
 
